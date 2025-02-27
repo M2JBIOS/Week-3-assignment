bd281e516e0acf9106abf0e7f7e37f59ab61c4d2
M2JBIOS <deverj415@gmail.com>
2025-02-27 18:18:51 -0500

    Add files via upload

---
+++ assignment_3.py
@@ -0,0 +1,47 @@
+##### ASSIGNMENT STARTS HERE #####
+
+
+#%%
+# First Assignment
+'''
+1: Please create a program which inputs two years and outputs the years as well as the difference
+between them!
+
+Your output should look like:
+
+Year 1: 2025
+Year 2: 2028
+Difference: 3
+'''
+
+
+#%%
+# Second Assignment
+
+'''
+2: Please create a program which collects an input as fahrenheit and outputs the 
+temperature in celsius
+
+Your output should look like:
+
+Fahrenheit: 25
+Celsius: -3.89
+'''
+
+
+#%%
+# Third Assignment
+
+'''
+
+3: Please create a program which collects an input as US Dollars and converts the output to Euros 
+given the exchange rate on 1/19/25 as 1 USD = 0.97 Euro
+
+Your output should look like:
+USD: 1
+EU: 0.97
+
+'''
+
+
+##### ASSIGNMENT ENDS HERE #####
